b359754ab1a3006bacab1b0261420017d63ee104
SoleilDivin <vinesh.sinel@gmail.com>
2022-12-13 09:26:35 +0300

    fix: change CORS configuration to allow http://localhost:8100 Change CORS configuration to support ionic default port

--- src/main/java/flotte/FlotteApplication.java
+++ src/main/java/flotte/FlotteApplication.java
@@ -3,6 +3,7 @@ package flotte;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.lang.NonNull;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -17,8 +18,8 @@ public class FlotteApplication {
     WebMvcConfigurer corsConfig() {
         return new WebMvcConfigurer() {
             @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/*").allowedOriginPatterns("*");
+            public void addCorsMappings(@NonNull CorsRegistry registry) {
+                registry.addMapping("/*").allowedOriginPatterns("http://localhost:8100");
             }
         };
     }
